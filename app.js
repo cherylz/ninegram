4c3aae509d21c1762b11435fa60d5d47d0817331
cz <deutschessz@gmail.com>
2019-04-11 16:03:40 +0800

    Abstract code & Add more checks to user search input

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const objectFitSupported = document.body.style.objectFit !== undefined ? true : false;
 const promptMsg = document.querySelector('.instruction > p');
 const modal = document.querySelector('.modal');
+let searchFromUser = false;
 let easterEggShown = false;
 let scrolling = false;
 let currentImageIndex;
 let currentSearchTerm;
 
 // If the easter egg was shown to the user during previous page visit(s), then it won't be displayed.
-// Must be executed before updatePage(searchTerm, '') because updatePage needs the updated value of easterEggShown.
+// Must be executed before conductSearch(searchTerm) because conductSearch needs the updated value of easterEggShown.
 const easter = localStorage.getItem('easterEggShown');
 if (easter) {
   easterEggShown = true;
@@ -16,7 +17,7 @@ if (easter) {
 // On page load, update the page with images fetched from API and add event listeners.
 const randomTerms = ['architecture', 'city', 'mountain', 'london', 'moon'];
 const searchTerm = randomTerms[Math.floor(Math.random() * randomTerms.length)];
-updatePage(searchTerm, '');
+conductSearch(searchTerm);
 
 // Handle the keyup event when user pressed the enter key in the search box.
 const inputBox = document.querySelector('input[type=text]');
@@ -52,7 +53,8 @@ document.addEventListener('click', function(e) {
     handleLightboxClose();
   }
 
-  if (!e.target.matches('.gallery-img') && modal.className === 'modal') {
+  const inLightboxView = modal.className === 'modal' ? true : false; // In other words, if the modal's class name is 'modal no-display', it means the lightbox is hidden.
+  if (!e.target.matches('.gallery-img') && inLightboxView) {
     decideWhetherToCloseLightbox(e);
   }
 
@@ -63,7 +65,8 @@ document.addEventListener('click', function(e) {
 
 // Use event delegation to handle keydown events in the lightbox view.
 document.addEventListener('keydown', function(e) {
-  if (modal.className === 'modal') {
+  const inLightboxView = modal.className === 'modal' ? true : false;
+  if (inLightboxView) {
     if (e.key === 'ArrowLeft' || e.key === 'Left') {
       e.preventDefault(); // Prevent horizontal scrolling.
       handleSlideScroll(-1);
@@ -103,6 +106,8 @@ setInterval(() => {
 
 // Below are the functions to be called.
 
+// -> Functions for lightbox:
+
 function showSlide() {
   const slides = document.querySelectorAll('.slide');
   slides.forEach((slide, index) => {
@@ -132,7 +137,7 @@ function handleLightboxClose() {
   promptMsg.textContent = 'Hope you enjoyed the images!';
 
   // Close the lightbox.
-  document.querySelector('.modal').className = 'modal no-display';
+  modal.className = 'modal no-display';
 }
 
 function decideWhetherToCloseLightbox(e) {
@@ -148,132 +153,160 @@ function decideWhetherToCloseLightbox(e) {
   }
 }
 
-function updatePage(term, from) {
-  if (easterEggShown) {
-    document.querySelector('.easter-egg').innerHTML = '';
+// -> Functions for image search:
+
+function handleError(err) {
+  if (searchFromUser) {
+    promptMsg.className = '';
+    promptMsg.textContent =
+      'Oops... Something went wrong. Perhaps try another search term, or just enjoy the existing images below. :)';
+  } else {
+    promptMsg.className = 'prompt-color';
+    promptMsg.textContent = 'Oops... Something went wrong. Please refresh the page.';
+  }
+  console.log(err, err.message);
+}
+
+function UpdatePage(result) {
+  // Render proper prompt message.
+  if (searchFromUser) {
+    promptMsg.className = '';
+    promptMsg.textContent =
+      'Nice search! Click an image to zoom in and scroll through, or get new images with another search.';
   }
 
-  if (from === 'user') {
+  // Render the 9 images fetched.
+  const gallery = document.querySelector('.gallery-container');
+  const galleryClass = objectFitSupported ? 'gallery-item' : 'gallery-item-ie';
+  const htmlInGallery = result.reduce(
+    (str, item, index) =>
+      str +
+      `<div class="${galleryClass} cell-${index + 1}">
+        <img
+          src=${item.webformatURL.replace('_640', '_340')}
+          alt="${item.tags}"
+          data-index=${index}
+          class="gallery-img">
+      </div>`,
+    ''
+  );
+  gallery.innerHTML = htmlInGallery;
+
+  // Update content inside slideshow to be displayed in the lightbox view.
+  const slideshow = document.querySelector('.slideshow');
+  slideshow.innerHTML = '';
+  const slideImgClass = objectFitSupported ? 'slide-img' : 'slide-img-ie';
+  const htmlInSlideshow = result.reduce(
+    (str, item, index) =>
+      str +
+      `<div class="slide">
+        <img
+          src=${item.webformatURL.replace('_640', '_960')}
+          alt="${item.tags}"
+          class=${slideImgClass}>
+        <div class="overlay">
+          tags: ${item.tags}
+        </div>
+      </div>`,
+    ''
+  );
+  slideshow.innerHTML = htmlInSlideshow;
+
+  // Upon the successful default search on page load, all the searches afterwards will be from user.
+  if (!searchFromUser) {
+    searchFromUser = true;
+  }
+
+  // Update easter egg.
+  if (easterEggShown) {
+    document.querySelector('.easter-egg').innerHTML = '';
+  } else {
+    document.querySelector('.easter-egg').innerHTML = '<button>Load More</button>';
+  }
+}
+
+function checkImageQtyThenUpdatePage(res) {
+  const result = res.hits;
+  if (result.length < 9) {
+    if (searchFromUser) {
+      promptMsg.className = '';
+      promptMsg.textContent =
+        'Oops... Less than 9 images found. Why not try another search term? You can also just enjoy the existing images below. :)';
+      return;
+    } else {
+      conductSearch('lights'); // Handle a rare case: the initial search on page load got less than 9 images to render.
+      return;
+    }
+  }
+  UpdatePage(result);
+}
+
+function validateResponse(res) {
+  if (res.ok) {
+    return res.json();
+  }
+  if (searchFromUser) {
+    promptMsg.className = '';
+    promptMsg.textContent =
+      'Oops... Something went wrong. Perhaps try another search term, or just enjoy the existing images below. :)';
+  } else {
+    promptMsg.className = 'prompt-color';
+    promptMsg.textContent = 'Oops... Something went wrong. Please refresh the page.';
+  }
+  throw Error(res.statusText);
+}
+
+function conductSearch(term) {
+  // Update the value of currentSearchTerm.
+  currentSearchTerm = term;
+
+  // Update prompt message.
+  if (searchFromUser) {
     promptMsg.className = 'prompt-color short';
     promptMsg.textContent = 'Searching...';
   }
 
-  fetch(
-    `https://pixabay.com/api/?key=11381563-185a2b7b89dac3ac1a22ea903&q=${term}&image_type=photo&min_width=295&min_height=295&per_page=9&editors_choice=true&safesearch=true`
-  )
-    .then(res => {
-      if (res.ok) {
-        return res.json();
-      }
-      if (from === 'user') {
-        promptMsg.className = '';
-        promptMsg.textContent =
-          'Oops... Something went wrong. Perhaps try another search term, or just enjoy the existing images below. :)';
-      } else {
-        promptMsg.className = 'prompt-color';
-        promptMsg.textContent = 'Oops... Something went wrong. Please refresh the page.';
-      }
-      throw Error(res.statusText);
-    })
-    .then(res => {
-      // Handle the case when less than 9 images are fetched
-      const results = res.hits;
-      if (results.length < 9) {
-        if (from === 'user') {
-          promptMsg.className = '';
-          promptMsg.textContent =
-            'Oops... Less than 9 images found. Why not try another search term? You can also just enjoy the existing images below. :)';
-          return;
-        } else {
-          updatePage('lights', ''); // Handle a rare case: the initial search on page load got less than 9 images to render.
-          return;
-        }
-      }
-
-      // Render images
-      const gallery = document.querySelector('.gallery-container');
-      const galleryClass = objectFitSupported ? 'gallery-item' : 'gallery-item-ie';
-      const htmlInGallery = results.reduce(
-        (str, item, index) =>
-          str +
-          `<div class="${galleryClass} cell-${index + 1}">
-            <img
-              src=${item.webformatURL.replace('_640', '_340')}
-              alt="${item.tags}"
-              data-index=${index}
-              class="gallery-img">
-          </div>`,
-        ''
-      );
-      gallery.innerHTML = htmlInGallery;
-
-      // Render proper prompt message.
-      if (from === 'user') {
-        promptMsg.className = '';
-        promptMsg.textContent =
-          'Nice search! Click an image to zoom in and scroll through, or get new images with another search.';
-      }
-
-      // Update content inside the slideshow.
-      const slideshow = document.querySelector('.slideshow');
-      slideshow.innerHTML = '';
-      const slideImgClass = objectFitSupported ? 'slide-img' : 'slide-img-ie';
-      const htmlInSlideshow = results.reduce(
-        (str, item, index) =>
-          str +
-          `<div class="slide">
-            <img
-              src=${item.webformatURL.replace('_640', '_960')}
-              alt="${item.tags}"
-              class=${slideImgClass}>
-            <div class="overlay">
-              tags: ${item.tags}
-            </div>
-          </div>`,
-        ''
-      );
-      slideshow.innerHTML = htmlInSlideshow;
-
-      // Render the fake load more button.
-      if (!easterEggShown) {
-        document.querySelector('.easter-egg').innerHTML = '<button>Load More</button>';
-      }
-    })
-    .catch(err => {
-      if (from === 'user') {
-        promptMsg.className = '';
-        promptMsg.textContent =
-          'Oops... Something went wrong. Perhaps try another search term, or just enjoy the existing images below. :)';
-      } else {
-        promptMsg.className = 'prompt-color';
-        promptMsg.textContent = 'Oops... Something went wrong. Please refresh the page.';
-      }
-      console.log(err, err.message);
-    });
+  // Fetch results from API and update page accordingly.
+  const endpoint = `https://pixabay.com/api/?key=11381563-185a2b7b89dac3ac1a22ea903&q=${term}&image_type=photo&min_width=295&min_height=295&per_page=9&editors_choice=true&safesearch=true`;
+  fetch(endpoint)
+    .then(validateResponse)
+    .then(checkImageQtyThenUpdatePage)
+    .catch(handleError);
 }
 
-function handleSearch(e) {
-  const searchTerm = document.querySelector('input[type=text]').value;
+function handleSearch() {
+  const cleanedInput = document.querySelector('input[type=text]').value.trim();
+  const lettersExp = /^[A-Za-z\s]+$/;
 
-  if (!searchTerm) {
+  if (!cleanedInput) {
     return;
   }
 
-  if (searchTerm === currentSearchTerm) {
+  if (cleanedInput === currentSearchTerm) {
     promptMsg.className = 'prompt-color short';
     promptMsg.textContent = 'Perhaps try another search term?';
     return;
   }
 
-  if (searchTerm.length <= 100) {
-    currentSearchTerm = searchTerm;
-    updatePage(searchTerm, 'user');
-  } else {
+  if (!cleanedInput.match(lettersExp)) {
+    promptMsg.className = 'prompt-color short';
+    promptMsg.textContent =
+      'Hmm... Only a search term with letters and space is accepted.';
+    return;
+  }
+
+  if (cleanedInput.length > 100) {
     promptMsg.className = 'prompt-color';
     promptMsg.textContent =
       'Oops...The search term you entered is a bit long. Why not try a shorter one? :)';
+    return;
   }
+
+  // Finally, the search term is valid to trigger the actual search and corresponding actions.
+  conductSearch(cleanedInput);
 }
+
+// -> Other function(s):
 
 function handleEasterEgg() {
   document.querySelector('.easter-egg').innerHTML =
--- app_compiled.js
+++ app_compiled.js
@@ -1,13 +1,14 @@
-'use strict';
+"use strict";
 
 var objectFitSupported = document.body.style.objectFit !== undefined ? true : false;
 var promptMsg = document.querySelector('.instruction > p');
 var modal = document.querySelector('.modal');
+var searchFromUser = false;
 var easterEggShown = false;
 var scrolling = false;
 var currentImageIndex;
 var currentSearchTerm; // If the easter egg was shown to the user during previous page visit(s), then it won't be displayed.
-// Must be executed before updatePage(searchTerm, '') because updatePage needs the updated value of easterEggShown.
+// Must be executed before conductSearch(searchTerm) because conductSearch needs the updated value of easterEggShown.
 
 var easter = localStorage.getItem('easterEggShown');
 
@@ -15,18 +16,19 @@ if (easter) {
   easterEggShown = true;
 } // On page load, update the page with images fetched from API and add event listeners.
 
+
 var randomTerms = ['architecture', 'city', 'mountain', 'london', 'moon'];
 var searchTerm = randomTerms[Math.floor(Math.random() * randomTerms.length)];
-updatePage(searchTerm, ''); // Handle the keyup event when user pressed the enter key in the search box.
+conductSearch(searchTerm); // Handle the keyup event when user pressed the enter key in the search box.
 
 var inputBox = document.querySelector('input[type=text]');
-inputBox.addEventListener('keyup', function(e) {
+inputBox.addEventListener('keyup', function (e) {
   if (e.key === 'Enter') {
     handleSearch();
   }
 }); // Use event delegation to handle click events especially the events of dynamically rendered elements.
 
-document.addEventListener('click', function(e) {
+document.addEventListener('click', function (e) {
   if (e.target.closest('.search > button')) {
     handleSearch();
   }
@@ -51,7 +53,9 @@ document.addEventListener('click', function(e) {
     handleLightboxClose();
   }
 
-  if (!e.target.matches('.gallery-img') && modal.className === 'modal') {
+  var inLightboxView = modal.className === 'modal' ? true : false; // In other words, if the modal's class name is 'modal no-display', it means the lightbox is hidden.
+
+  if (!e.target.matches('.gallery-img') && inLightboxView) {
     decideWhetherToCloseLightbox(e);
   }
 
@@ -60,8 +64,10 @@ document.addEventListener('click', function(e) {
   }
 }); // Use event delegation to handle keydown events in the lightbox view.
 
-document.addEventListener('keydown', function(e) {
-  if (modal.className === 'modal') {
+document.addEventListener('keydown', function (e) {
+  var inLightboxView = modal.className === 'modal' ? true : false;
+
+  if (inLightboxView) {
     if (e.key === 'ArrowLeft' || e.key === 'Left') {
       e.preventDefault(); // Prevent horizontal scrolling.
 
@@ -80,10 +86,10 @@ document.addEventListener('keydown', function(e) {
   }
 }); // Make the navigation menu responsive to page scroll.
 
-window.addEventListener('scroll', function() {
-  return (scrolling = true);
+window.addEventListener('scroll', function () {
+  return scrolling = true;
 });
-setInterval(function() {
+setInterval(function () {
   if (scrolling) {
     scrolling = false;
 
@@ -102,10 +108,11 @@ setInterval(function() {
     }
   }
 }, 200); // Below are the functions to be called.
+// -> Functions for lightbox:
 
 function showSlide() {
   var slides = document.querySelectorAll('.slide');
-  slides.forEach(function(slide, index) {
+  slides.forEach(function (slide, index) {
     if (index === currentImageIndex) {
       slide.className = 'slide active';
     } else {
@@ -133,155 +140,146 @@ function handleLightboxClose() {
   promptMsg.className = 'short';
   promptMsg.textContent = 'Hope you enjoyed the images!'; // Close the lightbox.
 
-  document.querySelector('.modal').className = 'modal no-display';
+  modal.className = 'modal no-display';
 }
 
 function decideWhetherToCloseLightbox(e) {
   var targetClass = e.target.className;
 
-  if (
-    targetClass !== 'close' &&
-    targetClass !== 'prev' &&
-    targetClass !== 'next' &&
-    targetClass !== 'slide active' &&
-    e.target.parentNode.className !== 'slide active'
-  ) {
+  if (targetClass !== 'close' && targetClass !== 'prev' && targetClass !== 'next' && targetClass !== 'slide active' && e.target.parentNode.className !== 'slide active') {
     handleLightboxClose();
   }
+} // -> Functions for image search:
+
+
+function handleError(err) {
+  if (searchFromUser) {
+    promptMsg.className = '';
+    promptMsg.textContent = 'Oops... Something went wrong. Perhaps try another search term, or just enjoy the existing images below. :)';
+  } else {
+    promptMsg.className = 'prompt-color';
+    promptMsg.textContent = 'Oops... Something went wrong. Please refresh the page.';
+  }
+
+  console.log(err, err.message);
 }
 
-function updatePage(term, from) {
+function UpdatePage(result) {
+  // Render proper prompt message.
+  if (searchFromUser) {
+    promptMsg.className = '';
+    promptMsg.textContent = 'Nice search! Click an image to zoom in and scroll through, or get new images with another search.';
+  } // Render the 9 images fetched.
+
+
+  var gallery = document.querySelector('.gallery-container');
+  var galleryClass = objectFitSupported ? 'gallery-item' : 'gallery-item-ie';
+  var htmlInGallery = result.reduce(function (str, item, index) {
+    return str + "<div class=\"".concat(galleryClass, " cell-").concat(index + 1, "\">\n        <img\n          src=").concat(item.webformatURL.replace('_640', '_340'), "\n          alt=\"").concat(item.tags, "\"\n          data-index=").concat(index, "\n          class=\"gallery-img\">\n      </div>");
+  }, '');
+  gallery.innerHTML = htmlInGallery; // Update content inside slideshow to be displayed in the lightbox view.
+
+  var slideshow = document.querySelector('.slideshow');
+  slideshow.innerHTML = '';
+  var slideImgClass = objectFitSupported ? 'slide-img' : 'slide-img-ie';
+  var htmlInSlideshow = result.reduce(function (str, item, index) {
+    return str + "<div class=\"slide\">\n        <img\n          src=".concat(item.webformatURL.replace('_640', '_960'), "\n          alt=\"").concat(item.tags, "\"\n          class=").concat(slideImgClass, ">\n        <div class=\"overlay\">\n          tags: ").concat(item.tags, "\n        </div>\n      </div>");
+  }, '');
+  slideshow.innerHTML = htmlInSlideshow; // Upon the successful default search on page load, all the searches afterwards will be from user.
+
+  if (!searchFromUser) {
+    searchFromUser = true;
+  } // Update easter egg.
+
+
   if (easterEggShown) {
     document.querySelector('.easter-egg').innerHTML = '';
+  } else {
+    document.querySelector('.easter-egg').innerHTML = '<button>Load More</button>';
   }
-
-  if (from === 'user') {
-    promptMsg.className = 'prompt-color short';
-    promptMsg.textContent = 'Searching...';
-  }
-
-  fetch(
-    'https://pixabay.com/api/?key=11381563-185a2b7b89dac3ac1a22ea903&q='.concat(
-      term,
-      '&image_type=photo&min_width=295&min_height=295&per_page=9&editors_choice=true&safesearch=true'
-    )
-  )
-    .then(function(res) {
-      if (res.ok) {
-        return res.json();
-      }
-
-      if (from === 'user') {
-        promptMsg.className = '';
-        promptMsg.textContent =
-          'Oops... Something went wrong. Perhaps try another search term, or just enjoy the existing images below. :)';
-      } else {
-        promptMsg.className = 'prompt-color';
-        promptMsg.textContent = 'Oops... Something went wrong. Please refresh the page.';
-      }
-
-      throw Error(res.statusText);
-    })
-    .then(function(res) {
-      // Handle the case when less than 9 images are fetched
-      var results = res.hits;
-
-      if (results.length < 9) {
-        if (from === 'user') {
-          promptMsg.className = '';
-          promptMsg.textContent =
-            'Oops... Less than 9 images found. Why not try another search term? You can also just enjoy the existing images below. :)';
-          return;
-        } else {
-          updatePage('lights', ''); // Handle a rare case: the initial search on page load got less than 9 images to render.
-
-          return;
-        }
-      } // Render images
-
-      var gallery = document.querySelector('.gallery-container');
-      var galleryClass = objectFitSupported ? 'gallery-item' : 'gallery-item-ie';
-      var htmlInGallery = results.reduce(function(str, item, index) {
-        return (
-          str +
-          '<div class="'
-            .concat(galleryClass, ' cell-')
-            .concat(index + 1, '">\n            <img\n              src=')
-            .concat(item.webformatURL.replace('_640', '_340'), '\n              alt="')
-            .concat(item.tags, '"\n              data-index=')
-            .concat(index, '\n              class="gallery-img">\n          </div>')
-        );
-      }, '');
-      gallery.innerHTML = htmlInGallery; // Render proper prompt message.
-
-      if (from === 'user') {
-        promptMsg.className = '';
-        promptMsg.textContent =
-          'Nice search! Click an image to zoom in and scroll through, or get new images with another search.';
-      } // Update content inside the slideshow.
-
-      var slideshow = document.querySelector('.slideshow');
-      slideshow.innerHTML = '';
-      var slideImgClass = objectFitSupported ? 'slide-img' : 'slide-img-ie';
-      var htmlInSlideshow = results.reduce(function(str, item, index) {
-        return (
-          str +
-          '<div class="slide">\n            <img\n              src='
-            .concat(item.webformatURL.replace('_640', '_960'), '\n              alt="')
-            .concat(item.tags, '"\n              class=')
-            .concat(
-              slideImgClass,
-              '>\n            <div class="overlay">\n              tags: '
-            )
-            .concat(item.tags, '\n            </div>\n          </div>')
-        );
-      }, '');
-      slideshow.innerHTML = htmlInSlideshow; // Render the fake load more button.
-
-      if (!easterEggShown) {
-        document.querySelector('.easter-egg').innerHTML = '<button>Load More</button>';
-      }
-    })
-    ['catch'](function(err) {
-      if (from === 'user') {
-        promptMsg.className = '';
-        promptMsg.textContent =
-          'Oops... Something went wrong. Perhaps try another search term, or just enjoy the existing images below. :)';
-      } else {
-        promptMsg.className = 'prompt-color';
-        promptMsg.textContent = 'Oops... Something went wrong. Please refresh the page.';
-      }
-
-      console.log(err, err.message);
-    });
 }
 
-function handleSearch(e) {
-  var searchTerm = document.querySelector('input[type=text]').value;
+function checkImageQtyThenUpdatePage(res) {
+  var result = res.hits;
 
-  if (!searchTerm) {
+  if (result.length < 9) {
+    if (searchFromUser) {
+      promptMsg.className = '';
+      promptMsg.textContent = 'Oops... Less than 9 images found. Why not try another search term? You can also just enjoy the existing images below. :)';
+      return;
+    } else {
+      conductSearch('lights'); // Handle a rare case: the initial search on page load got less than 9 images to render.
+
+      return;
+    }
+  }
+
+  UpdatePage(result);
+}
+
+function validateResponse(res) {
+  if (res.ok) {
+    return res.json();
+  }
+
+  if (searchFromUser) {
+    promptMsg.className = '';
+    promptMsg.textContent = 'Oops... Something went wrong. Perhaps try another search term, or just enjoy the existing images below. :)';
+  } else {
+    promptMsg.className = 'prompt-color';
+    promptMsg.textContent = 'Oops... Something went wrong. Please refresh the page.';
+  }
+
+  throw Error(res.statusText);
+}
+
+function conductSearch(term) {
+  // Update the value of currentSearchTerm.
+  currentSearchTerm = term; // Update prompt message.
+
+  if (searchFromUser) {
+    promptMsg.className = 'prompt-color short';
+    promptMsg.textContent = 'Searching...';
+  } // Fetch results from API and update page accordingly.
+
+
+  var endpoint = "https://pixabay.com/api/?key=11381563-185a2b7b89dac3ac1a22ea903&q=".concat(term, "&image_type=photo&min_width=295&min_height=295&per_page=9&editors_choice=true&safesearch=true");
+  fetch(endpoint).then(validateResponse).then(checkImageQtyThenUpdatePage)["catch"](handleError);
+}
+
+function handleSearch() {
+  var cleanedInput = document.querySelector('input[type=text]').value.trim();
+  var lettersExp = /^[A-Za-z\s]+$/;
+
+  if (!cleanedInput) {
     return;
   }
 
-  if (searchTerm === currentSearchTerm) {
+  if (cleanedInput === currentSearchTerm) {
     promptMsg.className = 'prompt-color short';
     promptMsg.textContent = 'Perhaps try another search term?';
     return;
   }
 
-  if (searchTerm.length <= 100) {
-    currentSearchTerm = searchTerm;
-    updatePage(searchTerm, 'user');
-  } else {
-    promptMsg.className = 'prompt-color';
-    promptMsg.textContent =
-      'Oops...The search term you entered is a bit long. Why not try a shorter one? :)';
+  if (!cleanedInput.match(lettersExp)) {
+    promptMsg.className = 'prompt-color short';
+    promptMsg.textContent = 'Hmm... Only a search term with letters and space is accepted.';
+    return;
   }
-}
+
+  if (cleanedInput.length > 100) {
+    promptMsg.className = 'prompt-color';
+    promptMsg.textContent = 'Oops...The search term you entered is a bit long. Why not try a shorter one? :)';
+    return;
+  } // Finally, the search term is valid to trigger the actual search and corresponding actions.
+
+
+  conductSearch(cleanedInput);
+} // -> Other function(s):
+
 
 function handleEasterEgg() {
-  document.querySelector('.easter-egg').innerHTML =
-    "<p>You clicked the button, didn't you? They say less is more. So you are not getting more images. Good day ;)</p>";
+  document.querySelector('.easter-egg').innerHTML = "<p>You clicked the button, didn't you? They say less is more. So you are not getting more images. Good day ;)</p>";
   easterEggShown = true;
   localStorage.setItem('easterEggShown', true);
 }
